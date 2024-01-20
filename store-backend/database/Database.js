7eb38afab4d4f74893a9f5688beac752e988024d
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2024-01-20 10:53:03 -0500

    Added an install in dockerfile

--- store-backend/database/Database.js
+++ store-backend/database/Database.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('BurritoShop', 'postgres', 'admin', { //normally should use .env file to hide password
     host: 'localhost',
-    port: 5434,
+    // port: 5434,
     dialect: 'postgres'
-});
+})
 
 module.exports = sequelize;
